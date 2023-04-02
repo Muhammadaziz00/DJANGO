d14b80f7f8f9bb2f4987b0d6d041b0cd4c5256e1
Muhammadaziz00 <m.aziz.s.0727@gmail.com>
2023-04-02 19:12:36 +0600

    hm2

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,31 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.urls import reverse
 
-# Create your tests here.
+
+class HelloViewTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_hello(self):
+        response = self.client.get(reverse("hello-view "))
+        expected_date = "Hello"
+        self.assertEqual(expected_date, response.connect.decode())
+        self.assertEqual(500, response.status_code)
+
+class AboutViewTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_about(self):
+        response = self.client.get(reverse('about-views'))
+        excepted_data = "как-то так"
+        self.assertEqual(excepted_data, response.content.decode())
+
+class ContactsViewTestCase(TestCase):
+    def setup(self):
+        self.client = Client()
+    
+    def test_contacts(self):
+        response = self.client.get(reverse('contacts-views'))
+        excepted_data = "как-то так2"
+        self.assertEqual(excepted_data, response.content.decode())
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+# about/ -> "About"
+
+
+# request = объект класса  HtttpResponse который формирует из запроса от клиента на наш сервер 
+
+def hello(request):
+
+    return HttpResponse("Hello", headers = {"Name": "Alex"}, status = 500)
+
+def get_about(request):
+    return HttpResponse("как-то так1")
+
+def get_contacts(request):
+    return HttpResponse("как-то так")
