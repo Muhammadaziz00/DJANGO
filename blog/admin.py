35cfcf9610f4c8457ea709e8548372b227fd526b
Muhammadaziz00 <m.aziz.s.0727@gmail.com>
2023-04-19 18:59:36 +0600

    hm6

--- blog/admin.py
+++ blog/admin.py
@@ -1,12 +1,17 @@
-from django.contrib import admin
-from blog.models import Post
 
+from django.contrib import admin
+from blog.models import Post, Comment
+
+# Register your models here.
 
 # admin.site.register(Post)
-
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ["title", "created", "status"]
     list_filter = ["status"]
     list_editable = ["status"]
-    search_fields = ["title", "content"]
+    search_fields = ['title', 'content']
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ["post", "username", "created"]
--- blog/views.py
+++ blog/views.py
@@ -1,65 +1,74 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views import generic
-from blog.models import Post
 
+from blog.forms import CommentForm, PostForm
+from blog.models import Post, Comment
 
-# Read/Retrieve
 class IndexView(generic.ListView):
-    # model = Post
-    queryset = Post.objects.filter(status=True)
-    context_object_name = "posts"
+    # model = Post 
     template_name = "blog/index.html"
-    # extra_context = {"title": "Главная страница"}
+    queryset = Post.objects.filter(status=True)
+    context_object_name = 'posts'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = ''
+        return context
 
-
-# Read/Retrieve
-class PostDetailView(generic.DetailView):
-    model = Post
-    context_object_name = "post"
-    template_name = "blog/post_detail.html"
-
-
-# CREATE
 class PostCreateView(generic.CreateView):
     model = Post
-    template_name = "blog/post_create.html"
+    template_name = 'blog/post_create.html'
     success_url = reverse_lazy("index-page")
-    fields = ["title", "content"]
+    form_class = PostForm
 
 
-# DELETE
-class PostDeleteView(generic.DeleteView):
+
+
+#CREATE
+class PostDetailView(generic.DetailView):
     model = Post
+    context_object_name = 'post'
+    template_name = "blog/post_detail.html"
+    extra_context = {"form" : CommentForm()}
+
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     context["form"] = CommentForm()
+    #     return context
+
+    def post(self, request, pk):
+        post = Post.objects.get(pk=pk)
+        form  = CommentForm(request.POST)
+        if form.is_valid():
+            pre_saved_comment = form.save(commit=False)
+            pre_saved_comment.post = post
+            pre_saved_comment.save()
+        # username = request.POST.get("username_input", None)
+        # text = request.POST.get("text", None)
+        # if username and text != "":
+        #     print("У нас все есть")
+        #     comment = Comment.objects.create(username=username, text=text, post=post)
+        #     comment.save()
+        return redirect("post-detail", pk)
+
+#DELETE
+
+class PostDeleteConfirimView(generic.DeleteView):
+    model = Post
+    template_name = "blog/post_delete_confirm.html"
     success_url = reverse_lazy("index-page")
 
 
-# UPDATE
+#UPDATE
 class PostUpdateView(generic.UpdateView):
     model = Post
-    template_name = "blog/post_update.html"
-    success_url = reverse_lazy("index-page")
+    template_name = 'blog/post_update.html'
+    success_url = reverse_lazy("post-update")
     fields = ["title", "content"]
 
+class AboutView(generic.TemplateView):
+    template_name = "blog/about.html"
 
-# def get_index(request):
-#     posts = Post.objects.all()
-#     context = {
-#         "title": "Главная страница",
-#         "posts": posts,
-#     }
-#     return render(request, "blog/index.html", context=context)
-
-
-def get_about(request):
-    # context = {
-    #     "title": "Страница о нас"
-    # }
-    return render(request, "blog/about.html", context=None)
-
-
-def get_contacts(request):
-    context = {
-        "title": "Как с нами связаться"
-    }
-    return render(request, "blog/contacts.html", context=context)
+class ContactsView(generic.TemplateView):
+    template_name = "blog/contacts.html"
